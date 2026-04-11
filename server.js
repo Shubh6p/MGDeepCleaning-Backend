3f63d99e3ead60638c383a8dbc2c40bc8c9cf0af
Shubh6p <prasadshubhambsp06@gmail.com>
2026-04-11 20:03:59 +0530

    Update

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Cache-Control', 'Pragma']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
